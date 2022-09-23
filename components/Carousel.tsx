f596f59f8df9c6c65bb3867f4aa50054810a6855
Giuseppe Barillari <joe2k01dev@gmail.com>
2022-09-23 14:50:17 +0100

    Automatically change pictures in the carousel
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { IconContext } from "react-icons";
 import Image from "next/image";
@@ -8,8 +8,38 @@ type CarouselProps = {
   images: string[];
 };
 
-const Carousel: React.FC<CarouselProps> = ({ images }) => {
+type AutoIncrementProps = {
+  images: string[];
+};
+
+type AutoIncrementType = [number, () => void, () => void];
+
+const useAutoIncrement = ({
+  images,
+}: AutoIncrementProps): AutoIncrementType => {
   const [currentImage, setCurrentImage] = useState(0);
+  const nextImage = useCallback(() => {
+    currentImage !== images.length - 1
+      ? setCurrentImage((i) => i + 1)
+      : setCurrentImage(0);
+  }, [currentImage, images.length]);
+
+  const previousImage = useCallback(() => {
+    currentImage !== 0
+      ? setCurrentImage((i) => i - 1)
+      : setCurrentImage(images.length - 1);
+  }, [currentImage, images.length]);
+
+  useEffect(() => {
+    const t = setTimeout(() => nextImage(), 5000);
+    return () => clearTimeout(t);
+  }, [nextImage]);
+
+  return [currentImage, nextImage, previousImage];
+};
+
+const Carousel: React.FC<CarouselProps> = ({ images }) => {
+  const [currentImage, nextImage, previousImage] = useAutoIncrement({ images });
   const [carousel] = useAutoAnimate<HTMLDivElement>();
   return (
     <div className="flex flex-row p-2 w-full h-full justify-between items-center">
@@ -18,9 +48,7 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
       >
         <button
           onClick={() => {
-            currentImage !== 0
-              ? setCurrentImage((i) => i - 1)
-              : setCurrentImage(images.length - 1);
+            previousImage();
           }}
         >
           <MdChevronLeft />
@@ -45,9 +73,7 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
         </div>
         <button
           onClick={() => {
-            currentImage !== images.length - 1
-              ? setCurrentImage((i) => i + 1)
-              : setCurrentImage(0);
+            nextImage();
           }}
         >
           <MdChevronRight />
