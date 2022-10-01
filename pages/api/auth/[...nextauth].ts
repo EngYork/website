ed3ba076428b18d1f81772581abe24d710e182db
Giuseppe Barillari <joe2k01dev@gmail.com>
2022-10-01 18:13:51 +0100

    Set up schema for database auth
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,15 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import prisma from "../../../lib/prismadb";
 
 export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID || "",
       clientSecret: process.env.DISCORD_CLIENT_SECRET || "",
     }),
-    // ...add more providers here
   ],
 };
 export default NextAuth(authOptions);
