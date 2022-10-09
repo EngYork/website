f2d548556f35f786ceb78e71e64dacd29b1e89e0
Giuseppe Barillari <joe2k01dev@gmail.com>
2022-10-09 15:06:25 +0100

    Add header
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://shocksoc.github.io",
   base: "/website",
+  integrations: [tailwind()],
 });
