507b39273cc582c522bf0f81a06a61bd82366526
Giuseppe Barillari <joe2k01dev@gmail.com>
2022-10-17 11:23:32 +0100

    Compress assets and add description meta tag
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
+import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
-import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
